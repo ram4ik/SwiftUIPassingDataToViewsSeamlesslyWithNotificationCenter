bea1d463f5b253d80b75209b30aa2760f13a86f3
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-05-03 15:10:28 +0300

    Initial Commit

---
+++ SwiftUIPassingDataToViewsSeamlesslyWithNotificationCenter/SwiftUIPassingDataToViewsSeamlesslyWithNotificationCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPassingDataToViewsSeamlesslyWithNotificationCenterApp.swift
+//  SwiftUIPassingDataToViewsSeamlesslyWithNotificationCenter
+//
+//  Created by Ramill Ibragimov on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPassingDataToViewsSeamlesslyWithNotificationCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
